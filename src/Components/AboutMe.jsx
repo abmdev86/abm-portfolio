87cf78a782041aad68c00cdcdebe1308b2321a59
Auroiah Morgan <amwebdev@outlook.com>
2023-02-26 12:16:37 -0500

    Update AboutMe.jsx

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -2,9 +2,10 @@ import { Paper, Typography } from "@mui/material";
 
 export default function AboutMe() {
   return (
-    <Paper elevation={3} sx={{ m: 5, p: 4 }}>
+    <Paper elevation={3} sx={{ m: { xs: 2, md: 5 }, p: 4 }}>
       <Typography variant="h4" align="center">About Me:</Typography>
-      <Typography variant="body1" align="center" paragraph sx={{ letterSpacing: 1, lineHeight: 2, p: 3, fontWeight: 700 }} >
+
+      <Typography variant="body1" align="center" paragraph sx={{ letterSpacing: 1, lineHeight: 2, p: 3, fontWeight: 700, align: 'center' }} >
         Fullstack software engineer, with an interest in JavaScript, React and
         mobile development. Adept at various frameworks and CMS including React,
         Nextjs, WordPress as well as experience with Apache servers, PHP, SQL,
