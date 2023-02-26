130a96b79e21358a099abf839442e485b6cb4a59
Auroiah Morgan <amwebdev@outlook.com>
2023-02-26 12:16:41 -0500

    Update Layout.jsx

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -4,14 +4,20 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { mainTheme } from "../Themes";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import { Container } from "@mui/system";
 
 export default function Layout({ children }) {
     return (
         <ThemeProvider theme={mainTheme}>
             <CssBaseline enableColorScheme />
             <NavBar logoName="Auroiah" />
-            {children}
+
+            <Container maxWidth='md' sx={{ height: '100vh', m: 'auto' }}>
+
+                {children}
+            </Container>
             <Footer />
+
         </ThemeProvider>
     );
 }
