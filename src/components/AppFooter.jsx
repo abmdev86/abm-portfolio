fd7fcf0c53d23fab51913a41bb1d3ca43be59ba1
Auroiah Morgan <amwebdev@outlook.com>
2023-03-04 17:22:38 -0500

    Fix design layout

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,46 +1,46 @@
-import { Container, Typography, Box } from "@mui/material";
-import Grid from "@mui/material/Unstable_Grid2";
-import CopyrightIcon from '@mui/icons-material/Copyright';
+import { Typography, Paper, Container, Box } from '@mui/material';
 
+import CopyrightIcon from '@mui/icons-material/Copyright';
 
 export default function AppFooter() {
   return (
-    <Container maxWidth="sm">
-      <footer>
-        <Grid
-          container
-          spacing={2}
-          rowSpacing={1}
-          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          color="inherit"
-          sx={{ flexShrink: 0, position: 'relative', width: '100%', clear: 'both', mt: 4, bottom: 0 }}
-        >
-          <Grid xs={12}>
+    <Paper
+      sx={{
+        marginTop: 'calc(10% + 60px)',
+        position: 'fixed',
+        bottom: 0,
+        width: '100%',
+        textAlign: 'center',
+      }}
+      component="footer"
+      variant='outlined'
+    >
+      <Container maxWidth="md">
+        <Box sx={{
+          flexGrow: 1,
+          justifyContent: "center",
+          display: "flex",
+          my: 1
+        }}>
+          <Typography
+            variant="subtitle1"
+            sx={{
+              color: 'inherit',
+              letterSpacing: 2,
+              fontWeight: 600,
+              textAlign: 'center',
+            }}
+          >
+            <small>
+              Aurmor Designs
+              <CopyrightIcon />
+              2023
+            </small>
+          </Typography>
 
-            <Box sx={{ maxWidth: true }}>
-              <Typography variant="subtitle1" sx={{ color: 'inherit', letterSpacing: 2, fontWeight: 600, textAlign: 'center', }} >
+        </Box>
+      </Container>
 
-                <small>
-                  Aurmor Designs
-                  <CopyrightIcon />
-                  2023
-
-                </small>
-              </Typography>
-            </Box>
-
-
-
-          </Grid>
-          {/* <Grid xs={4}>
-            <img alt="shopify-logo" src={SHOPIFY_LOGO} />
-          </Grid> */}
-
-        </Grid>
-      </footer>
-
-    </Container>
-
-
+    </Paper>
   );
 }
